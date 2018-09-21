4d1ea1fb19492ae7ddd351a245a4ca676440d1df
kybs0 <1016724958@qq.com>
2018-09-21 23:08:28 +0800

    上传代码

---
+++ ComplexTextBlockDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComplexTextBlockDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("开启学霸模式！");
+        }
+        private void LinkedButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("iexplore.exe", "http://www.cnblogs.com/kybs0/");
+        }
+    }
+}
